d1c651cbfbd382ddc8eb15caca014dced2036067
zeller030 <zeller030@gmail.com>
2026-06-07 01:30:36 +0800

    fix the config.js

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -1,38 +1,35 @@
-/*
- * @Author: zeller030 zeller030@gmail.com
- * @Date: 2026-06-06 23:59:16
- * @LastEditors: zeller030 zeller030@gmail.com
- * @LastEditTime: 2026-06-07 00:27:25
- * @FilePath: \my-blog\docs\.vitepress\config.mjs
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- */
 import { defineConfig } from 'vitepress'
 
-// https://vitepress.dev/reference/site-config
 export default defineConfig({
-  base: '/my-blog/', 
-  title: "zeller's blog",
-  description: "personal blog",
+  base: '/my-blog/',
+  title: "Zeller's Blog",
+  description: "个人技术博客",
+
   themeConfig: {
-    // https://vitepress.dev/reference/default-theme-config
     nav: [
-      { text: 'Home', link: '/' },
-      { text: 'Examples', link: '/markdown-examples' }
+      { text: '首页', link: '/' },
+      { text: '文章', link: '/posts/' },
     ],
 
-    sidebar: [
-      {
-        text: 'Examples',
-        items: [
-          { text: 'Markdown Examples', link: '/markdown-examples' },
-          { text: 'Runtime API Examples', link: '/api-examples' },
-          { text: 'hello ', link: '/hello' }
-        ]
-      }
-    ],
+    sidebar: {
+      '/posts/': [
+        {
+          text: 'MCU',
+          items: [
+            {
+              text: 'ESP32',
+              items: [
+                { text: 'ESP32-S3 三子棋', link: '/posts/mcu/esp32/esp32-chess' },
+                { text: 'ESP32 步进电机', link: '/posts/mcu/esp32/esp32-stepper' },
+              ]
+            }
+          ]
+        }
+      ]
+    },
 
     socialLinks: [
-      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+      { icon: 'github', link: 'https://github.com/lewuq' }
     ]
   }
 })
